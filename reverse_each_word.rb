d2dcc5fe4d827e89f2b29fab49ec259e74a5983d
shmuly levitin <shmuly11@gmail.com>
2020-11-23 18:59:31 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,24 @@
+#require 'pry'
+def reverse_each_word(sentence1)
+ sentence = sentence1.split(" ")
+
+  sentence.each do |word|
+    word.reverse!
+   
+  end
+  new_sentence = sentence.join(" ")
+  # binding.pry
+new_sentence
+end
+
+def reverse_each_word(sentence2)
+ sentence = sentence2.split(" ")
+
+  sentence.collect do |word|
+    word.reverse!
+   
+  end
+  new_sentence = sentence.join(" ")
+  # binding.pry
+new_sentence
+end
